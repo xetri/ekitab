5a2417cb7c027d3f8a2617ca946bcb31ee14107b
xetri <avxetri@gmail.com>
2025-05-17 03:23:29 +0545

    TS build

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
-  target: "node20",
+  target: "es2020",
   format: ["esm"],
   sourcemap: false,
+  minify: true,
   clean: true,
   dts: false,
+  skipNodeModulesBundle: true,
   tsconfig: "./tsconfig.json",
 });
